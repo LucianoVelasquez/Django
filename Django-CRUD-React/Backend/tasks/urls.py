117e83235028d4c79da4ed8acbe0c6a5ce00196d
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-09-18 08:23:09 -0300

    add client

--- Django-CRUD-React/Backend/tasks/urls.py
+++ Django-CRUD-React/Backend/tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls #Modulo para documentar API
 from rest_framework import routers
 from tasks import views
 
@@ -8,6 +9,7 @@ router.register(r'tasks',views.TaskView, 'tasks')
 
 urlpatterns = [
     path('api/v1/',include(router.urls)),
+    path('docs/', include_docs_urls(title="Tasks API")) #Ruta que documenta toda la API
 ]
 
 #Todo este codigo ya esta generando las rutas GET, POST, PUT, DELETE
